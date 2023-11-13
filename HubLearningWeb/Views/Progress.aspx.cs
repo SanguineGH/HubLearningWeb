b6808f09f0bf602a332f2208f1ab2a4524d12334
SanguineGH <liccudjaymes@gmail.com>
2023-11-13 22:26:33 +0800

    Working

--- HubLearningWeb/Views/Progress.aspx.cs
+++ HubLearningWeb/Views/Progress.aspx.cs
@@ -38,18 +38,18 @@ namespace HubLearningWeb.Views
                 connection.Open();
 
                 string query = "SELECT t.tid AS TransactionID, " +
-                               "CASE WHEN b.role = 'Tutee' THEN ut.name ELSE tutor.name END AS TuteeName, " +
-                               "CASE WHEN b.role = 'Tutor' THEN ut.name ELSE tutor.name END AS TutorName, " +
-                               "CASE WHEN b.role = 'Tutee' THEN ut.studid ELSE tutor.studid END AS TuteeStudentID, " +
-                               "CASE WHEN b.role = 'Tutor' THEN ut.studid ELSE tutor.studid END AS TutorStudentID, " +
+                               "CASE WHEN b.role = 'Tutor' THEN ut.name ELSE tutor.name END AS TuteeName, " +
+                               "CASE WHEN b.role = 'Tutee' THEN ut.name ELSE tutor.name END AS TutorName, " +
+                               "CASE WHEN b.role = 'Tutor' THEN ut.studid ELSE tutor.studid END AS TuteeStudentID, " +
+                               "CASE WHEN b.role = 'Tutee' THEN ut.studid ELSE tutor.studid END AS TutorStudentID, " +
                                "b.yearlevel AS TuteeYearLevel, b.strand AS TuteeStrand, " +
                                "b.availability AS TutorAvailability, b.location AS TutorLocation, " +
                                "t.progress " +
                                "FROM transaction t " +
-                               "INNER JOIN bulletin b ON t.requestor = b.rid " +
+                               "INNER JOIN bulletin b ON t.requestor = b.rid OR t.client = b.rid " +
                                "INNER JOIN users ut ON t.client = ut.uid " +
                                "LEFT JOIN users tutor ON b.uid = tutor.uid " +
-                               "WHERE b.uid = @UID";
+                               "WHERE b.uid = @UID OR t.requestor = @UID OR t.client = @UID";
 
                 using (MySqlCommand cmd = new MySqlCommand(query, connection))
                 {
