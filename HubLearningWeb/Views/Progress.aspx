d0cc67139202e4314c0632a3d891e7be3880c260
SanguineGH <liccudjaymes@gmail.com>
2023-12-03 23:23:06 +0800

    Install-Package itext7
    Install-Package itext7.bouncy-castle-adapter
    
    Copy ninyo yan isa isa then ipaste ninyo sa Nuget Console Manager.
    
    One at a Time lang.

--- HubLearningWeb/Views/Progress.aspx
+++ HubLearningWeb/Views/Progress.aspx
@@ -67,32 +67,31 @@
                 </div>
             </div>
             <div class="Content">
+                <asp:Button ID="btnGeneratePDF" runat="server" Text="Generate PDF" OnClick="GeneratePDF_Click" />
                 <asp:GridView ID="progressGridView" runat="server" AutoGenerateColumns="False" OnRowCommand="progressGridView_RowCommand" DataKeyNames="TransactionID">
-                    <Columns>
-                        <asp:BoundField DataField="TuteeName" HeaderText="Tutee Name" SortExpression="TuteeName" />
-                        <asp:BoundField DataField="TuteeStudentID" HeaderText="Tutee Student ID" SortExpression="TuteeStudentID" />
-                        <asp:BoundField DataField="TutorName" HeaderText="Tutor Name" SortExpression="TutorName" />
-                        <asp:BoundField DataField="TutorStudentID" HeaderText="Tutor Student ID" SortExpression="TutorStudentID" />
-                        <asp:BoundField DataField="TuteeYearLevel" HeaderText="Year Level" SortExpression="TuteeYearLevel" />
-                        <asp:BoundField DataField="TuteeStrand" HeaderText="Strand" SortExpression="TuteeStrand" />
-                        <asp:BoundField DataField="TutorAvailability" HeaderText="Availability" SortExpression="TutorAvailability" />
-                        <asp:BoundField DataField="TutorLocation" HeaderText="Location" SortExpression="TutorLocation" />
-                        <asp:BoundField DataField="days" HeaderText="Days" SortExpression="days" />
-<asp:TemplateField HeaderText="Progress">
-    <ItemTemplate>
-        <asp:Literal ID="litProgress" runat="server"></asp:Literal>
-    </ItemTemplate>
-</asp:TemplateField>
-                          <asp:TemplateField HeaderText="Actions">
-                            <ItemTemplate>
-                                
-                                <asp:Button ID="btnMore" runat="server" Text="More" CssClass="more-button" CommandName="MoreCommand" />
-                                <asp:HiddenField ID="hfRowIndex" runat="server" Value='<%# Container.DataItemIndex %>' />
-                            </ItemTemplate>
-                        </asp:TemplateField>
-                    </Columns>
-                </asp:GridView>
+    <Columns>
+        <asp:BoundField DataField="TransactionID" HeaderText="Transaction ID" SortExpression="TransactionID" />
+        <asp:BoundField DataField="TuteeName" HeaderText="Tutee Name" SortExpression="TuteeName" />
+        <asp:BoundField DataField="TuteeStudentID" HeaderText="Tutee Student ID" SortExpression="TuteeStudentID" />
+        <asp:BoundField DataField="TutorName" HeaderText="Tutor Name" SortExpression="TutorName" />
+        <asp:BoundField DataField="TutorStudentID" HeaderText="Tutor Student ID" SortExpression="TutorStudentID" />
+        <asp:BoundField DataField="TuteeYearLevel" HeaderText="Year Level" SortExpression="TuteeYearLevel" />
+        <asp:BoundField DataField="TuteeStrand" HeaderText="Strand" SortExpression="TuteeStrand" />
+        <asp:BoundField DataField="TutorAvailability" HeaderText="Availability" SortExpression="TutorAvailability" />
+        <asp:BoundField DataField="TutorLocation" HeaderText="Location" SortExpression="TutorLocation" />
+        <asp:BoundField DataField="days" HeaderText="Days" SortExpression="days" />
+        <asp:BoundField DataField="progress" HeaderText="Progress" SortExpression="progress" />
+        <asp:TemplateField HeaderText="Actions">
+            <ItemTemplate>
+                <asp:Button ID="btnMore" runat="server" Text="More" CssClass="more-button" CommandName="MoreCommand" />
+                <asp:HiddenField ID="hfRowIndex" runat="server" Value='<%# Container.DataItemIndex %>' />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
                
+                
+
                 
                 
                 <div id="additionalContent" class="additional-content" runat="server" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border: 1px solid #ccc;">
--- HubLearningWeb/Views/Progress.aspx.cs
+++ HubLearningWeb/Views/Progress.aspx.cs
@@ -8,6 +8,11 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
 using System.Web.UI.HtmlControls;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using System.IO;
+using iText.Layout;
+
 
 namespace HubLearningWeb.Views
 {
@@ -34,29 +39,56 @@ namespace HubLearningWeb.Views
         {
             string connectionString = "Server=localhost;Database=learninghubwebdb;Uid=root;Pwd=;";
             string uid = Session["UID"].ToString();
+            int sessionKey = Convert.ToInt32(Session["SessionKey"]);
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
 
-                string query = "SELECT t.tid AS TransactionID, " +
-                "CASE WHEN b.role = 'Tutor' THEN ut.name ELSE tutor.name END AS TuteeName, " +
-                "CASE WHEN b.role = 'Tutee' THEN ut.name ELSE tutor.name END AS TutorName, " +
-                "CASE WHEN b.role = 'Tutor' THEN ut.studid ELSE tutor.studid END AS TuteeStudentID, " +
-                "CASE WHEN b.role = 'Tutee' THEN ut.studid ELSE tutor.studid END AS TutorStudentID, " +
-                "b.yearlevel AS TuteeYearLevel, b.strand AS TuteeStrand, " +
-                "b.availability AS TutorAvailability, b.location AS TutorLocation, " +
-                "t.days, " + // Include the 'days' column
-                "t.progress " +
-                "FROM transaction t " +
-                "INNER JOIN bulletin b ON t.requestor = b.rid OR t.client = b.rid " +
-                "INNER JOIN users ut ON t.client = ut.uid " +
-                "LEFT JOIN users tutor ON b.uid = tutor.uid " +
-                "WHERE b.uid = @UID OR t.requestor = @UID OR t.client = @UID";
+                string query;
+
+                if (sessionKey == 1)
+                {
+                    // If the session key is 1, retrieve all entries
+                    query = "SELECT t.tid AS TransactionID, " +
+                            "CASE WHEN b.role = 'Tutor' THEN ut.name ELSE tutor.name END AS TuteeName, " +
+                            "CASE WHEN b.role = 'Tutee' THEN ut.name ELSE tutor.name END AS TutorName, " +
+                            "CASE WHEN b.role = 'Tutor' THEN ut.studid ELSE tutor.studid END AS TuteeStudentID, " +
+                            "CASE WHEN b.role = 'Tutee' THEN ut.studid ELSE tutor.studid END AS TutorStudentID, " +
+                            "b.yearlevel AS TuteeYearLevel, b.strand AS TuteeStrand, " +
+                            "b.availability AS TutorAvailability, b.location AS TutorLocation, " +
+                            "t.days, " + // Include the 'days' column
+                            "t.progress " +
+                            "FROM transaction t " +
+                            "INNER JOIN bulletin b ON t.requestor = b.rid OR t.client = b.rid " +
+                            "INNER JOIN users ut ON t.client = ut.uid " +
+                            "LEFT JOIN users tutor ON b.uid = tutor.uid";
+                }
+                else
+                {
+                    // If the session key is not 1, retrieve entries related to the user
+                    query = "SELECT t.tid AS TransactionID, " +
+                            "CASE WHEN b.role = 'Tutor' THEN ut.name ELSE tutor.name END AS TuteeName, " +
+                            "CASE WHEN b.role = 'Tutee' THEN ut.name ELSE tutor.name END AS TutorName, " +
+                            "CASE WHEN b.role = 'Tutor' THEN ut.studid ELSE tutor.studid END AS TuteeStudentID, " +
+                            "CASE WHEN b.role = 'Tutee' THEN ut.studid ELSE tutor.studid END AS TutorStudentID, " +
+                            "b.yearlevel AS TuteeYearLevel, b.strand AS TuteeStrand, " +
+                            "b.availability AS TutorAvailability, b.location AS TutorLocation, " +
+                            "t.days, " + // Include the 'days' column
+                            "t.progress " +
+                            "FROM transaction t " +
+                            "INNER JOIN bulletin b ON t.requestor = b.rid OR t.client = b.rid " +
+                            "INNER JOIN users ut ON t.client = ut.uid " +
+                            "LEFT JOIN users tutor ON b.uid = tutor.uid " +
+                            "WHERE b.uid = @UID OR t.requestor = @UID OR t.client = @UID";
+                }
 
                 using (MySqlCommand cmd = new MySqlCommand(query, connection))
                 {
-                    cmd.Parameters.AddWithValue("@UID", uid);
+                    if (sessionKey != 1)
+                    {
+                        cmd.Parameters.AddWithValue("@UID", uid);
+                    }
 
                     using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
                     {
@@ -182,5 +214,90 @@ namespace HubLearningWeb.Views
                 lblTopMiddle.Text = dayInformation;
             }
         }
+
+        protected void GeneratePDF_Click(object sender, EventArgs e)
+        {
+            // Create a MemoryStream to hold the PDF
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Create a PdfWriter instance
+                using (PdfWriter writer = new PdfWriter(ms))
+                {
+                    // Create a PdfDocument instance
+                    using (var pdf = new PdfDocument(writer))
+                    {
+                        // Create a Document instance
+                        using (var document = new Document(pdf))
+                        {
+                            // Add content to the document
+                            document.Add(new Paragraph($"Transaction Progress Report ({DateTime.Now})").SetBold());
+
+                            // Add GridView content to the document
+                            AddGridViewToDocument(document);
+
+                            // Save the document
+                            document.Close();
+                        }
+                    }
+                }
+
+                // Save the PDF to the response stream
+                Response.ContentType = "application/pdf";
+                Response.AddHeader("content-disposition", "attachment;filename=TransactionReport.pdf");
+                Response.OutputStream.Write(ms.ToArray(), 0, ms.ToArray().Length);
+            }
+        }
+
+        private void AddGridViewToDocument(Document document)
+        {
+            // Iterate through GridView rows and add content to the document
+            foreach (GridViewRow row in progressGridView.Rows)
+            {
+                // Extract data from GridView row
+                var transactionID = row.Cells[0].Text;
+                var tuteeName = row.Cells[1].Text;
+                var tutorName = row.Cells[3].Text;
+                var tutorStudentID = row.Cells[4].Text;
+                var tuteeStudentID = row.Cells[2].Text;
+                var strand = row.Cells[6].Text;
+                var yearLevel = row.Cells[5].Text;
+                var availabilities = row.Cells[7].Text;
+                var location = row.Cells[8].Text;
+                var days = row.Cells[9].Text;
+                var progress = row.Cells[10].Text;
+                var transactionDate = GetTransactionDate(transactionID); // Assuming you have a method to get the date
+
+                // Add data to the document in the specified format
+                var formattedData = $"Transaction ID: {transactionID} - Tutor Name: {tutorName} - Tutor StudentID: {tutorStudentID} - " +
+                                    $"Tutee Name: {tuteeName} - Tutee StudentID: {tuteeStudentID} - Strand: {strand} - " +
+                                    $"Year Level: {yearLevel} - Availabilities: {availabilities} - Location: {location} - " +
+                                    $"Days: {days} - Progress: {progress} - Date: {transactionDate}";
+
+                document.Add(new Paragraph(formattedData).SetBold());
+            }
+        }
+
+        private string GetTransactionDate(string transactionID)
+        {
+            string connectionString = "Server=localhost;Database=learninghubwebdb;Uid=root;Pwd=;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT trandate FROM transaction WHERE tid = @TransactionID";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@TransactionID", transactionID);
+
+                    object result = cmd.ExecuteScalar();
+
+                    // Check if the result is DBNull.Value before converting to string
+                    return result != DBNull.Value ? Convert.ToDateTime(result).ToString("yyyy-MM-dd") : string.Empty;
+                }
+            }
+        }
+
     }
 }
